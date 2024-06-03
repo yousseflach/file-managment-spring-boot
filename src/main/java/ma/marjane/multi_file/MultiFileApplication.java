db301fe30bbab31dfe5b47ecdc23c396a79c33b2
yousseflach <lachgar.youssef6174@gmail.com>
2024-06-03 09:56:59 +0100

    initail project

---
+++ src/main/java/ma/marjane/multi_file/MultiFileApplication.java
@@ -0,0 +1,13 @@
+package ma.marjane.multi_file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/marjane/multi_file/MultiFileApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.marjane.multi_file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
